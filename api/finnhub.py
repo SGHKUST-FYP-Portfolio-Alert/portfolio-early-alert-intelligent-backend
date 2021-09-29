f0916f9ec0ab563f4cfcf46f9f882090eeb33297
Ng Ka Fong <richardng1126@gmail.com>
2021-09-29 17:12:41 +0800

    wip: code to get news in batches

--- api/finnhub.py
+++ api/finnhub.py
@@ -1,5 +1,28 @@
 import finnhub
+from datetime import datetime, timedelta
+import time
 
 finnhub_client = finnhub.Client(api_key="c59shsqad3i93kd215jg")
 
-#finnhub_client.company_news('AAPL', _from="2021-09-28", to="2021-09-29")
+def fetch_stock_news(symbol):
+
+    news = []
+    new_news = None
+    start_date = datetime.utcnow().date() - timedelta(days=366)
+    end_date = datetime.utcnow().date()
+
+    while new_news != []:
+
+        if new_news is not None:
+            time.sleep(1)
+        
+        new_news = finnhub_client.company_news(symbol, _from=start_date, to=end_date.isoformat())
+        news += new_news
+        end_date = min(
+            end_date - timedelta(days=1),
+            datetime.utcfromtimestamp(news[-1]['datetime']).date()
+        )
+    
+    news_df = pd.DataFrame(news).drop_duplicates()
+
+    return news_df
