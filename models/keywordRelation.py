afe457984f3cc67ae37d3bd640713eeaa651347d
poswalabhishek <poswalabhishek1234@icloud.com>
2021-11-11 00:13:36 +0800

    initial commit and brainstorming

---
+++ models/keywordRelation.py
@@ -0,0 +1,10 @@
+# brainstorming how to determine relationship of words with the sentiment
+
+'''
+call the finbert model to get the sentiment
+for each retrieved sentiment (especially the ones that are very high and low towards the spectrum) for a news segment:
+    go through the news and find out the main keywords:
+        call the heatmap from keywordModelling.py to get a view
+
+Researched LDA: Does not seem much reliable.
+'''
