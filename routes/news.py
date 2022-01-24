f8da06b463ec84cdb62c02b379c476f33a7e34f0
Ng Ka Fong <richardng1126@gmail.com>
2022-01-24 14:25:49 +0800

    get_news support query parameters 'date'

--- routes/news.py
+++ routes/news.py
@@ -7,10 +7,11 @@ from schemas.news import News
 router = APIRouter()
 
 @router.get("", response_model=List[News])
-def get_news(counterparty: str = None, skip: int = 0, limit: int = 0):
+def get_news(counterparty: str = None, skip: int = 0, limit: int = 0, date: str = None):
+    news_filter = {}
     if counterparty is not None:
-        news_filter = {'counterparty': counterparty}
-    else:
-        news_filter = {}
+        news_filter['counterparty'] =  counterparty
+    if date is not None:
+        news_filter['date'] = date
     return list(db.get_news(news_filter, skip=skip, limit=limit, sort=True))
     
