9718ab15a1270fa870227393ed004cb894f05bb9
Ng Ka Fong <richardng1126@gmail.com>
2022-02-13 21:50:08 +0800

    aggregate keywords count bugfix: counting news including the keywords instead of $mergeObjects

--- calculations/aggregate.py
+++ calculations/aggregate.py
@@ -56,9 +56,30 @@ def aggregate_sentiments_daily():
 
 def aggregate_keywords_news_count_daily():
 
-    pipeline =  [
-        {'$group': {'_id': {'date': '$date', 'counterparty':'$counterparty'}, 'keyword_count': {'$mergeObjects': '$keyword_count'}, 'news_count': {'$sum':1}}},
-        {'$project': {'_id': 0, 'date':'$_id.date', 'counterparty':'$_id.counterparty', 'keyword_count': 1, 'news_count': 1}}
+    pipeline = [
+        { '$addFields': {'keywords': {'$objectToArray': '$keyword_count'}} },
+        { '$unwind': '$keywords'},
+        { '$group': {
+            '_id': {
+                'counterparty': '$counterparty',
+                'date': '$date',
+                'k': "$keywords.k"
+            },
+            'v': {'$sum': 1},
+        } },
+        { '$group': {
+            '_id': {
+                'counterparty': '$_id.counterparty',
+                'date': '$_id.date',
+            },
+            'keywords': { '$push': {'k': "$_id.k", 'v': "$v"}},
+        }},
+        { '$project': {
+            '_id': 0,
+            'counterparty': '$_id.counterparty',
+            'date': '$_id.date',
+            'keyword_count': {'$arrayToObject': "$keywords"}
+        }}
     ]
 
     return db.aggregate_news(pipeline)
