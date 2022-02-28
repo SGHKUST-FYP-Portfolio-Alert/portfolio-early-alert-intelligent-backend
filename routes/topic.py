d4b2182464f9a9164fd8fbcd84b971437cd60f71
Ng Ka Fong <richardng1126@gmail.com>
2022-02-28 16:20:31 +0800

    routes /topic GET, POST and DELTE

---
+++ routes/topic.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.encoders import jsonable_encoder
+from database import database as db
+from schemas.topic import Topic, TopicCreate
+from typing import List
+from ext_api.finnhub_wrapper import finnhub_client
+from bson.objectid import ObjectId
+
+router = APIRouter()
+
+@router.get("", response_model=List[Topic])
+def get_topics():
+    result = []
+    for topic in db.get_topics():
+        topic['id'] = str(topic['_id'])
+        result.append(topic)
+    print(topic)
+    return result
+
+@router.get("/", response_model=Topic)
+def get_topic(id: str):
+    id = ObjectId(id)
+    result = db.get_topic(id=id)
+    result['id'] = str(result['_id'])
+    return result
+
+@router.post("", status_code=201)
+def add_topic(topic: TopicCreate):
+    topic = jsonable_encoder(topic)
+    db.add_topic(topic)
+
+@router.put("")
+def update_topic(topic: Topic):
+    topic = jsonable_encoder(topic)
+    topic['_id'] = ObjectId(topic['id'])
+    del topic['id']
+    db.update_topic(topic)
+
+
+@router.delete("")
+def delete_topic(id: str):
+    id = ObjectId(id)
+    db.delete_topic(id)
