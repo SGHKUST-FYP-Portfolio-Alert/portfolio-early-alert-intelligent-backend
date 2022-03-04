8883f6d9d417e29d8745f828fc7f13f9219ad236
Walkier <chowwalker@gmail.com>
2022-03-04 20:59:36 +0800

    chore: del old files

--- models/keywordMapping.py
+++
@@ -1,7 +0,0 @@
-from transformers import BertTokenizer, BertForSequenceClassification
-import numpy as np
-import torch
-
-finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone', num_labels=3).to(device)
-tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
-
