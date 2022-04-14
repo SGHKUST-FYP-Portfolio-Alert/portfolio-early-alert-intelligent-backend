07a205edd2214b920df1feb84244fb2a566cbce1
Walkier <chowwalker@gmail.com>
2022-04-14 10:33:08 +0800

    fix: ensure v2

--- cron.py
+++ cron.py
@@ -114,7 +114,7 @@ def add_sentiment():
     # end_dt = datetime(2021, 10, 16)
     # unix_end_dt = end_dt.replace(tzinfo=timezone.utc).timestamp()
     logger.info("Start adding sentiment")
-    filter = {"sentiment":{"$exists": False}}
+    filter = {"v2":{"$exists": False}}
     update_size = 512 #infer news by batch to prevent infinite stuck, also affects num of embedding held in mem bf db update
 
     while db.get_news(filter).count():
